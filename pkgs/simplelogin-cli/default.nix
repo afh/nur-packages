c4df8f622d09a8f9f930dfaba3419d5829f2ea28
Alexis Hildebrandt <afh@surryhill.net>
2026-06-09 13:51:14 +0200

    simplelogin-cli: fix build

--- pkgs/simplelogin-cli/default.nix
+++ pkgs/simplelogin-cli/default.nix
@@ -3,19 +3,19 @@
   buildGoModule,
   fetchFromGitHub,
   installShellFiles,
+  nix-update-script,
+  versionCheckHook,
 }:
 
 buildGoModule (finalAttrs: {
   pname = "simplelogin-cli";
   version = "0.4.0";
-
   __structuredAttrs = true;
-  __darwinAllowLocalNetworking = true;
 
   src = fetchFromGitHub {
     owner = "mexcool";
     repo = "simplelogin-cli";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-Gs3hOoRBzDXXbRgs0Pykobxlzt1qbFzsIjQpcIoo6iE=";
   };
 
@@ -30,8 +30,6 @@ buildGoModule (finalAttrs: {
     "-X=main.date=1970-01-01T00:00:00Z"
   ];
 
-  subPackages = [ "." ];
-
   nativeBuildInputs = [
     installShellFiles
   ];
@@ -42,11 +40,22 @@ buildGoModule (finalAttrs: {
 
   postInstall = ''
     installManPage man/*.[1-9]
+    rm $out/bin/gen-man
+    ln -s sl $out/bin/simplelogin-cli
   '';
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "SimpleLogin cli to manage email aliases from the terminal.";
     homepage = "https://github.com/mexcool/simplelogin-cli";
+    changelog = "https://github.com/mexcool/simplelogin-cli/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ afh ];
     mainProgram = "sl";
